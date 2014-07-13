19bd0b8d3b8336f74539488fcb34a8fc647573bb
Mario Lavandero <mario@mlavandero.org>
2014-07-13 18:52:05 -0400

    Removes destroy action from item resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :clients, except: [:index, :destroy] do
     resources :sales, except: :destroy do
-      resources :items
+      resources :items, except: :destroy
     end
   end
 
