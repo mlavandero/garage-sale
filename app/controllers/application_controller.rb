e2da04c90f5474a61e2939e7dd19435dfe5d341f
Mario Lavandero <mario@mlavandero.org>
2014-07-28 00:58:48 -0400

    Configuration for custom redirect for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Configures redirect after the sign in
+  def after_sign_in_path_for(resource)
+    client_path(resource)
+  end
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource)
+    new_client_session_path
+  end
 end
