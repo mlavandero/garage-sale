370bd54ae17fef374a8c247dab6e040e779ce9c0
Mario Lavandero <mlavandero@icloud.com>
2014-11-28 23:09:10 -0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20140720221326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "clients", force: true do |t|
     t.string   "email"
