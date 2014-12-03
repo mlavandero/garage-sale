e73816b90603eb39153755cd23f3c05cb3185517
Mario Lavandero <mlavandero@icloud.com>
2014-12-02 23:49:27 -0300

    alias current_user with current_client

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  alias_method :current_user, :current_client
+
   # Configures redirect after the sign in
   def after_sign_in_path_for(resource)
     client_path(resource)
