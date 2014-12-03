45678bb6183a756e0d3f9d2fb9d3efdf7558bc3d
Mario Lavandero <mlavandero@icloud.com>
2014-12-02 23:24:47 -0300

    Removes validations for clients
    This validations weren’t needed to sign up
    
    Closes #9

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,9 +4,8 @@ class Client < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :sales
 
-  validates_presence_of :first_name, :last_name
   validates :email, presence: true, format: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
   validates_uniqueness_of :email, on: :create
-  validates :mobile_phone, presence: true, numericality: { only_integer: true }, length: { is: 8 }
+  validates :mobile_phone, numericality: { only_integer: true }, length: { is: 8 }, allow_nil: true, allow_blank: true
   validates :landline_phone, length: { in: 7..8 }, allow_nil: true, allow_blank: true
 end
